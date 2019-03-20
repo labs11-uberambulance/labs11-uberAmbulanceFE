12b2d4eaf785b499b7457c3148e4520bed8b6c1c
kjohna <43828763+kjohna@users.noreply.github.com>
2019-03-20 15:22:49 -0600

    setup OnboardingView

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
+import "./App.css";
+
+import OnboardingView from "./views/OnboardingView/OnboardingView";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         Client
+        <Route exact path="/onboarding" component={OnboardingView} />
       </div>
     );
   }
