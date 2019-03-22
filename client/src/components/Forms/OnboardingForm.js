16897ec3d9ad0c11878ec358fa3b9322ca10466e
kjohna <43828763+kjohna@users.noreply.github.com>
2019-03-22 08:16:57 -0600

    fix one more error from merging

--- client/src/components/Forms/OnboardingForm.js
+++ client/src/components/Forms/OnboardingForm.js
@@ -3,9 +3,6 @@ import axios from "../../axios-instance";
 import TextField from "@material-ui/core/TextField";
 import Grid from "@material-ui/core/Grid";
 import Button from "@material-ui/core/Button";
-import TextField from "@material-ui/core/TextField";
-import Grid from "@material-ui/core/Grid";
-import Button from "@material-ui/core/Button";
 import { fetchOnboardingForm } from "./FormsTemplate";
 
 class OnboardingForm extends React.Component {
