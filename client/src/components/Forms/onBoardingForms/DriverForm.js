21e003af3d5410efa9a231ebc4091fc5bc00755e
WebMats <lorlando127@gmail.com>
2019-03-23 11:25:39 -0400

    made 3 seperate forms for onboarding a user based on type

---
+++ client/src/components/Forms/onBoardingForms/DriverForm.js
@@ -0,0 +1,119 @@
+import React, { Component } from 'react'
+
+import TextField from '@material-ui/core/TextField';
+import MaskedInput from 'react-text-mask';
+import InputAdornment from '@material-ui/core/InputAdornment'
+import { Button } from '@material-ui/core';
+import './onBoardingForm.css'
+
+
+function TextMaskCustom(props) {
+    const { inputRef, ...other } = props;
+    
+    return (
+      <MaskedInput
+        {...other}
+        ref={ref => {
+          inputRef(ref ? ref.inputElement : null);
+        }}
+        mask={['(', /[1-9]/, /\d/, /\d/, ')', ' ', /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/]}
+        placeholderChar={'\u2000'}
+        showMask
+      />
+    );
+  }
+
+export default class OnBoardingForm extends Component {
+    constructor(props) {
+        super(props)
+        this.nameInp = React.createRef();
+        this.emailInp = React.createRef();
+        this.phoneInp = React.createRef();
+        this.addressInp = React.createRef();
+        this.rateInp = React.createRef();
+    }
+
+    scrollToNextInputHandler = (nextInp) => {
+        nextInp.current.focus({ preventScroll: true })
+        nextInp.current.scrollIntoView({behavior: 'smooth', block: 'center'});
+    }
+    onPressEnterHandler = (e, nextInp) => {
+        if (e.key === 'Enter') {
+            nextInp.current.focus({ preventScroll: true })
+            nextInp.current.scrollIntoView({behavior: 'smooth', block: 'center'});
+        }
+    }
+    submitForm = () => {
+        const formValues = {
+            type: 'drivers',
+            name: this.nameInp.current.value,
+            email: this.emailInp.current.value,
+            phone: this.phoneInp.current.value,
+            address: this.addressInp.current.value,
+            rate: this.rateInp.current.value
+        }
+        console.log(formValues)
+    }
+  render() {
+    return (
+      <div>
+        <div className="inputHolder">
+            <TextField
+                autoFocus
+                label="Full Name"
+                required
+                inputRef={this.nameInp}
+                fullWidth
+                onKeyPress={(e) => this.onPressEnterHandler(e, this.emailInp)}
+            />
+            <Button type="button" onClick={() => this.scrollToNextInputHandler(this.emailInp)}>Next</Button>
+        </div>
+        <div className="inputHolder" >
+            <TextField
+                label="Email"
+                required
+                fullWidth
+                inputRef={this.emailInp}
+                onKeyPress={(e) => this.onPressEnterHandler(e, this.phoneInp)}
+            />
+            <Button type="button" onClick={() => this.scrollToNextInputHandler(this.phoneInp)}>Next</Button>
+        </div>
+        <div className="inputHolder">
+            <TextField
+              label="Phone Number"
+              required
+              InputProps={{placeholder:'(  )    -    ', inputComponent:TextMaskCustom}}
+              fullWidth
+              inputRef={this.phoneInp}
+              onKeyPress={(e) => this.onPressEnterHandler(e, this.addressInp)}
+              helperText="This will be the number that mothers will use to contact you."
+            />
+            <Button type="button" onClick={() => this.scrollToNextInputHandler(this.addressInp)}>Next</Button>
+        </div>
+        <div className="inputHolder">
+            <TextField
+              label="Address"
+              required
+              fullWidth
+              inputRef={this.addressInp}
+              onKeyPress={(e) => this.onPressEnterHandler(e, this.rateInp)}
+            />
+            <Button type="button" onClick={() => this.scrollToNextInputHandler(this.rateInp)}>Next</Button>
+        </div>
+        <div className="inputHolder">
+            <TextField
+              label="Rate per 10km"
+              required
+              fullWidth
+              InputProps={{
+                startAdornment: <InputAdornment position="start">$</InputAdornment>,
+              }}
+              inputRef={this.rateInp}
+              helperText="We recommend $2, you pledge to never charge more."
+            />
+            <Button type="button" color="secondary" onClick={this.submitForm}>Submit</Button>
+        </div>
+      </div>
+    )
+  }
+}
---
+++ client/src/components/Forms/onBoardingForms/MotherForm.js
@@ -0,0 +1,138 @@
+import React, { Component } from 'react'
+
+import TextField from '@material-ui/core/TextField';
+import MaskedInput from 'react-text-mask';
+import { Button } from '@material-ui/core';
+import './onBoardingForm.css'
+
+
+function TextMaskCustom(props) {
+    const { inputRef, ...other } = props;
+    
+    return (
+      <MaskedInput
+        {...other}
+        ref={ref => {
+          inputRef(ref ? ref.inputElement : null);
+        }}
+        mask={['(', /[1-9]/, /\d/, /\d/, ')', ' ', /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/]}
+        placeholderChar={'\u2000'}
+        showMask
+      />
+    );
+  }
+
+
+export default class OnBoardingForm extends Component {
+    constructor(props) {
+        super(props)
+        this.nameInp = React.createRef();
+        this.emailInp = React.createRef();
+        this.phoneInp = React.createRef();
+        this.townInp = React.createRef();
+        this.descInp = React.createRef();
+        this.dueDateHolder = React.createRef();
+        this.dueDateInp = React.createRef();
+    }
+
+    scrollToNextInputHandler = (nextInp) => {
+        if (nextInp.current.type === 'date') {
+            nextInp.current.scrollIntoView({behavior: 'smooth', block: 'start'});
+        }
+        nextInp.current.focus({ preventScroll: true })
+        nextInp.current.scrollIntoView({behavior: 'smooth', block: 'center'});
+    }
+    onPressEnterHandler = (e, nextInp) => {
+        if (e.key === 'Enter') {
+            this.scrollToNextInputHandler(nextInp)
+        }
+    }
+    submitForm = () => {
+        const formValues = {
+            type: 'mothers',
+            name: this.nameInp.current.value,
+            email: this.emailInp.current.value,
+            phone: this.phoneInp.current.value,
+            town: this.townInp.current.value,
+            description: this.descInp.current.value,
+            dueDate: this.dueDateInp.current.value
+        }
+        this.nameInp.current.value = ''
+        this.emailInp.current.value = ''
+        this.phoneInp.current.value = ''
+        this.townInp.current.value = ''
+        this.descInp.current.value = ''
+        this.dueDateHolder.current.value = ''
+        this.dueDateInp.current.value = ''
+        console.log(formValues)
+    }
+  render() {
+    return (
+      <div>
+        <div className="inputHolder">
+            <TextField
+                autoFocus
+                label="Full Name"
+                required
+                inputRef={this.nameInp}
+                fullWidth
+                onKeyPress={(e) => this.onPressEnterHandler(e, this.emailInp)}
+            />
+            <Button type="button" onClick={() => this.scrollToNextInputHandler(this.emailInp)}>Next</Button>
+        </div>
+        <div className="inputHolder" >
+            <TextField
+                label="Email"
+                fullWidth
+                inputRef={this.emailInp}
+                onKeyPress={(e) => this.onPressEnterHandler(e, this.phoneInp)}
+            />
+            {this.emailInp.current === false && <p>We good</p>}
+            <Button type="button" onClick={() => this.scrollToNextInputHandler(this.phoneInp)}>Next</Button>
+        </div>
+        <div className="inputHolder">
+            <TextField
+              label="Phone Number"
+              InputProps={{placeholder:'(  )    -    ', inputComponent:TextMaskCustom}}
+              fullWidth
+              inputRef={this.phoneInp}
+              onKeyPress={(e) => this.onPressEnterHandler(e, this.townInp)}
+              helperText="If you plan to use SMS to request transport this is required."
+            />
+            <Button type="button" onClick={() => this.scrollToNextInputHandler(this.townInp)}>Next</Button>
+        </div>
+        <div className="inputHolder">
+            <TextField
+              label="Nearest Town to Home"
+              required
+              fullWidth
+              inputRef={this.townInp}
+              onKeyPress={(e) => this.onPressEnterHandler(e, this.descInp)}
+            />
+            <Button type="button" onClick={() => this.scrollToNextInputHandler(this.descInp)}>Next</Button>
+        </div>
+        <div className="inputHolder">
+            <TextField
+              label="Further description to destination"
+              required
+              fullWidth
+              multiline
+              rows="4"
+              inputRef={this.descInp}
+            />
+            <Button type="button" onClick={() => this.scrollToNextInputHandler(this.dueDateHolder)}>Next</Button>
+        </div>
+        <div className="inputHolder" ref={this.dueDateHolder}>
+            <TextField
+                type="date"
+                label="Due Date"
+                InputLabelProps={{ shrink: true }}
+                fullWidth
+                inputRef={this.dueDateInp}
+            />
+            <Button type="button" color="secondary" onClick={this.submitForm}>Submit</Button>
+        </div>
+      </div>
+    )
+  }
+}
