342332b5358746e0551f2a1841b5f672981b5edc
kjohna <43828763+kjohna@users.noreply.github.com>
2019-03-29 15:15:07 -0600

    onbrd action creates user type record before updating user record

--- client/src/store/actions/onbrd.js
+++ client/src/store/actions/onbrd.js
@@ -18,32 +18,8 @@ export const initOnbrd = (user, formValues) => dispatch => {
     hospital
   } = formValues;
   // a user record was created automatically with first login but has no user info besides firebase ID.
-  // first update user record:
-  // map formValues to api format:
-  const userData = {
-    user: {
-      name,
-      phone,
-      user_type: type,
-      address: description,
-      village: town,
-      email
-    }
-  };
-  console.log("PUT data: ", userData);
-  axios
-    .put(`/api/users/update/${user.user.id}`, userData)
-    .then(res => {
-      console.log(`success updating user record: ${res.body}`);
-    })
-    .catch(err => {
-      console.log("error updating user record: ", err);
-      dispatch({
-        type: onbrdTypes.ONBRD_FAIL,
-        error: err
-      });
-    });
-  // second create user_type record:
+  // the api checks if user type is set and will not allow creating a user type record if it is. Therefore, create the user type record before updating user record.
+  // first create user type record:
   let typeData;
   if (formValues.type === "mothers") {
     typeData = {
@@ -67,12 +43,36 @@ export const initOnbrd = (user, formValues) => dispatch => {
   console.log("POST data: ", typeData);
   axios
     .post(`/api/users/onboard/${user.user.id}`, typeData)
-    .then(result => {
-      // PUT to /api/users/onboard/id will create a mother/driver record (based on user.user.type) with form values
-      // returns user data
+    .then(res => {
+      // POST to /api/users/onboard/id will create a mother/driver record (based on user.user.type) with form values
+      // update the user record once it's done:
+      // map formValues to api format:
+      const userData = {
+        user: {
+          name,
+          phone,
+          user_type: type,
+          address: description,
+          village: town,
+          email
+        }
+      };
+      console.log("PUT data: ", userData);
+      axios
+        .put(`/api/users/update/${user.user.id}`, userData)
+        .then(res => {
+          console.log(`success updating user record: ${res.body}`);
+        })
+        .catch(err => {
+          console.log("error updating user record: ", err);
+          dispatch({
+            type: onbrdTypes.ONBRD_FAIL,
+            error: err
+          });
+        });
       const payload = {
         ...user,
-        ...result.data
+        ...res.data
       };
       dispatch({
         type: onbrdTypes.ONBRD_SUCCESS,
