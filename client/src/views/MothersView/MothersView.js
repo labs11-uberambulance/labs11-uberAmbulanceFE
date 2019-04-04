b02d7344e1c6554a72edb693d9fb8946586e5217
WebMats <lorlando127@gmail.com>
2019-04-04 15:53:19 -0400

    added dummy data on request driver

--- client/src/views/MothersView/MothersView.js
+++ client/src/views/MothersView/MothersView.js
@@ -4,12 +4,21 @@ import axios from '../../axios-instance';
 import MotherHomePage from '../../components/Mothers/HomePage'
 
 
-const start = { lng: 1.0968556, lat: 33.4300912, location: 'Kakoge, Uganda' }
-const end = { lng: 0.3287080,lat: 32.557992,name: 'Hospital Name',distance: '10km' }
+const start =`${33.4300912},${1.0968556}`
+const end = `${32.557992},${0.3287080}` 
+const hospital = 'Hospital Name';
+const name = 'Lauren'
+const distance = '10';
+const phone = '+11111111111'
 
 function MothersView() {
   const requestDriverHandler = () => {
-    axios.post('/api/rides/request/driver/VhuBXHKe4cQFvdpwOMpqhGv2EHx1', {start, end}).then(res => {
+    axios.post('/api/rides/request/driver/VhuBXHKe4cQFvdpwOMpqhGv2EHx1', 
+    {
+      start, end, 
+      name, phone, 
+      distance, hospital 
+    }).then(res => {
       console.log(res)
     }).catch(err => {
       console.log(err)
