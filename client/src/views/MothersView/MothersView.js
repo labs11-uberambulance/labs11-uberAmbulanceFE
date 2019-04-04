406b428688fc9ce3e08f4ebe8823443a329199ee
WebMats <lorlando127@gmail.com>
2019-04-04 11:17:03 -0400

    handling driver request from mother

--- client/src/views/MothersView/MothersView.js
+++ client/src/views/MothersView/MothersView.js
@@ -1,14 +1,23 @@
 import React from "react";
 import { connect } from "react-redux";
+import axios from '../../axios-instance';
 import MotherHomePage from '../../components/Mothers/HomePage'
 
 
+const start = { lng: 1.0968556, lat: 33.4300912, location: 'Kakoge, Uganda' }
+const end = { lng: 0.3287080,lat: 32.557992,name: 'Hospital Name',distance: '10km' }
 
 function MothersView() {
-
+  const requestDriverHandler = () => {
+    axios.post('/api/rides/request/driver/VhuBXHKe4cQFvdpwOMpqhGv2EHx1', {start, end}).then(res => {
+      console.log(res)
+    }).catch(err => {
+      console.log(err)
+    })
+  }
   return (<>
   <MotherHomePage/>
-  
+    <button onClick={requestDriverHandler}>Request Driver</button>
   </>)
 }
 
