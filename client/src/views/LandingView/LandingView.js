052a48e33e390dac82df85073d4e4b508e2bde17
WebMats <lorlando127@gmail.com>
2019-04-08 19:30:00 -0400

    cleaned App.js and styled auth form view

---
+++ client/src/views/LandingView/LandingView.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../../components/LandingPage/Header/Header';
+import './LandingView.css';
+
+const LandingView = () => {
+  return (
+    <div className="landing-container">
+        <Header />
+    </div>
+  )
+}
+
+export default LandingView;
