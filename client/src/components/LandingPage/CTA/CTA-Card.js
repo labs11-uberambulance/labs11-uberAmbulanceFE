775f0fa75aaef55d1ae28967f95def3f50fae4e6
Matt Basile <matthewbasile2@gmail.com>
2019-04-11 16:33:42 -0400

    Landing page updated. Content in place for how does it work, header and cards. Needs to be styled and need to add a focus section and footer

---
+++ client/src/components/LandingPage/CTA/CTA-Card.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import MomIcon from './icons/icons8-pregnant-filled-50.png'
+import DriverIcon from './icons/icons8-ambulance-50.png'
+
+
+const styles = {
+  card: {
+    minWidth: 275,
+    width: "15%",
+    margin: "0 10px",
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+  button:{
+    width: "80%",
+    margin: "0 auto",
+    background: "red",
+  }
+};
+
+function SimpleCard(props) {
+  const { classes } = props;
+  const bull = <span className={classes.bullet}>•</span>;
+
+  return (
+    <Card className={classes.card}>
+      <CardContent>
+        <Typography variant="h5" component="h2">
+        {props.mother ? 'Mother' : 'Driver'}
+        </Typography>
+        <Typography className={classes.pos} color="textSecondary">
+        <img src={props.mother ? MomIcon : DriverIcon}/>
+        </Typography>
+        <Typography component="p">
+          well meaning and kindly.
+          <br />
+          {'"a benevolent smile"'}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button className={classes.button} size="large">Register Now</Button>
+      </CardActions>
+    </Card>
+  );
+}
+
+SimpleCard.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(SimpleCard);
