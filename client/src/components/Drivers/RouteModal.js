9cbbfdd44424f3dc4471d4dea4abc2bc979c15b7
kjohna <43828763+kjohna@users.noreply.github.com>
2019-04-17 11:42:55 -0600

    remove unused code

--- client/src/components/Drivers/RouteModal.js
+++ client/src/components/Drivers/RouteModal.js
@@ -1,21 +1,7 @@
 import React from "react";
-import PropTypes from "prop-types";
-import { withStyles } from "@material-ui/core/styles";
-import Typography from "@material-ui/core/Typography";
 import Modal from "@material-ui/core/Modal";
 import Button from "@material-ui/core/Button";
 import RouteMap from "../../components/GoogleMaps/RouteMap/RouteMap";
-
-const styles = theme => ({
-  paper: {
-    // position: 'absolute',
-    // width: theme.spacing.unit * 50,
-    // backgroundColor: theme.palette.background.paper,
-    // boxShadow: theme.shadows[5],
-    // padding: theme.spacing.unit * 4,
-    // outline: 'none',
-  }
-});
 
 class SimpleModal extends React.Component {
   constructor(props) {
@@ -34,8 +20,6 @@ class SimpleModal extends React.Component {
   };
 
   render() {
-    const { classes } = this.props;
-
     return (
       <div>
         <Button color="primary" onClick={this.handleOpen} fullWidth>
@@ -61,11 +45,4 @@ class SimpleModal extends React.Component {
   }
 }
 
-SimpleModal.propTypes = {
-  classes: PropTypes.object.isRequired
-};
-
-// We need an intermediary variable for handling the recursive nesting.
-const SimpleModalWrapped = withStyles(styles)(SimpleModal);
-
-export default SimpleModalWrapped;
+export default SimpleModal;
