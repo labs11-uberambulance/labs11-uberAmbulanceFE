58bcfa8d78bb779c8499475a576b74985f6a9f5e
WebMats <lorlando127@gmail.com>
2019-04-18 18:52:45 -0400

    added 'download app' button to landing page header

--- client/src/components/LandingPage/Header/Header.js
+++ client/src/components/LandingPage/Header/Header.js
@@ -6,6 +6,7 @@ const header = (props) => {
   return (
     <header className="landing-header">
         <nav>
+            <button className="landing-header--app-button"><a href="https://play.google.com/store/apps?hl=en_US" target="_black" rel="noopener noreferrer" >Download the App</a></button>
             <ul>
                 <li><NavLink to="/register">Sign up</NavLink></li>
                 <li><button onClick={() => {props.history.push('/login')}} >Log in</button></li>
